f6bf1c9fe663715c9ed55a46d593db02717524cb
BBX <bbx@bbx.com>
2025-11-17 12:10:47 +0800

    V4

--- worker.js
+++ worker.js
@@ -6,56 +6,64 @@ self.addEventListener('install', ev => {
 });
 self.addEventListener('activate', ev => ev.waitUntil(clients.claim()));
 self.addEventListener('fetch', ev => ev.respondWith((() => {
-    if (/sw\/$/.test(new URL(ev.request.url).pathname)) {
-        let [[field, value]] = new URLSearchParams(new URL(ev.request.url).search);
-        return (actions[field]?.[value] ?? actions[field]?._)?.(value)
-            .then(() => new Response('', {status: 200}))
-            .catch(er => console.error(er) ?? new Response('', {status: 400})) ?? new Response('', {status: 404});
-    }
-    return (is.internal(ev.request.url) ? caches.match(ev.request.url, {ignoreSearch: true}) : Promise.resolve())
-        .then(cached => {
-            if (cached && (is.part(ev.request.url) || is.stable(ev.request.url)))
+    let req = ev.request;
+    let action = new URLPattern({pathname: '/x/sw/'}).exec(req.url);
+    if (action)
+        return actions(action.search.groups[0].split('='));
+
+    return caches.match(req.url, {ignoreSearch: true})
+        .then(async cached => {
+            if (cached && is.part(req.url))
                 return cached;
-            let fetching = fetch.net(ev.request);
-            return cached ? is.html(ev.request.url) ? Head.add(cached) : cached : fetching;
-        }).catch(console.error);
+            let fetched = fetch.net(req);
+            cached || (fetched = await fetched);
+            return is.html(req.url) ? Head.add(cached || fetched) : cached || fetched;
+        }).catch(er => {
+            if (`${er}`.includes('Failed to fetch')) return;
+            console.error(req.url);
+            console.error(er);
+            new URLPattern({hostname: location.host, pathname: '/'}).test(url) && self.registration.unregister();
+        });
 })()));
 
-const actions = {
+const actions = ([field, value]) => (actions[field]?.[value] ?? actions[field]?._)?.(value)
+    .then(() => new Response('', {status: 200}))
+    .catch(er => console.error(er) ?? new Response('', {status: 400})) 
+    ?? new Response('', {status: 404});
+
+Object.assign(actions, {
     delete: {
         parts: () => fetch('db/-update.json').then(() => caches.delete('X/parts')),
         head: () => caches.open('X').then(cache => cache.add(Head.url)),
-        _: extension => fetch('db/-update.json')
+        _: file => fetch('db/-update.json')
             .then(() => caches.open('X'))
-            .then(cache => cache.keys().then(reqs => reqs.forEach(req => new RegExp(`\\.${extension}$`).test(req.url) && cache.delete(req))))
+            .then(cache => cache.keys().then(reqs => reqs.forEach(req => new RegExp(`\\.${file}$`).test(req.url) && cache.delete(req))))
     }
-}
+});
 
 const is = {
-    internal: url => ['aeoq.github.io', new URL(location.href).host].includes(new URL(url).host),
-    cacheable: url => (is.internal(url) || /cdn\.?js/.test(url)) && !/\.json$/.test(new URL(url).pathname),
-    stable: url => /bg.mp4$/.test(new URL(url).pathname),
-    volatile: url => /\.(?:css|js|json)$|head\.html$/.test(new URL(url).pathname),
-    part: url => /img\/.+?\/.+?\.png$/.test(new URL(url).pathname),
-    html: url => /(?:\/|\.html)$/.test(new URL(url).pathname),
-    opaque: url => /takaratomy/.test(url) ? {mode: 'no-cors'} : 
-        /aeoq\.github\.io.+\.css/.test(url) ? {mode: 'cors', credentials: 'omit'} : null
+    internal: url => location.host == new URL(url).host,
+    cacheable: url => is.internal(url) && !/\.json$/.test(new URL(url).pathname) 
+        || ['fonts.googleapis.com', 'aeoq.github.io'].includes(new URL(url).host) 
+        || [/cdn\.?js/, /firacode/].some(r => r.test(url)),
+    volatile: url => is.internal(url) && /\.(?:js|css|json)$/.test(new URL(url).pathname),
+    part: url => is.internal(url) && new URLPattern({pathname: '/x/img/*/*.png'}).test(url),
+    html: url => new URLPattern({pathname: '*(/|.html)'}).test(url),
 }
-fetch.net = req => {
-    is.internal(req.url) && is.volatile(req.url) && (req = new Request(`${req.url}?${Math.random()}`, req));
-    return fetch(req, is.opaque(req.url)).then(res => 
-        (res.status < 400 && is.cacheable(req.url) ? fetch.cache(res) : Promise.resolve(res))
-        .then(res => is.html(req.url) ? Head.add(res) : res)
-    ).catch(er => {
-        if (`${er}`.includes('Failed to fetch') || req.url.includes('mp4')) return;
-        console.error(req.url);
-        console.error(er);
-        new URL(req.url).pathname == '/' && self.registration.unregister();
+const to = {
+    random: req => new Request(`${req.url}?${Math.random()}`, req),
+    stripped: res => res.url.replace(/[?#].*$/, ''),
+    opaque: req => /takaratomy/.test(req.url) ? {mode: 'no-cors'} : 
+        new URLPattern({hostname: 'aeoq.github.io', pathname: '*.css'}).test(req.url) ? {mode: 'cors', credentials: 'omit'} : null
+}
+fetch.net = req => fetch(is.volatile(req.url) ? to.random(req) : req, to.opaque(req))
+    .then(res => {
+        if (!res.url || !res.ok || res.status == 206 || !is.cacheable(req.url))
+            return res;
+        let cloned = res.clone();
+        caches.open(is.part(res.url) ? 'X/parts' : 'X').then(cache => cache.put(to.stripped(res), cloned)); 
+        return res;
     });
-}
-fetch.cache = res => res.url ? caches.open(is.part(res.url) ? 'X/parts' : 'X')
-    .then(cache => cache.put(res.url.replace(/[?#].*$/, ''), res.clone()))
-    .then(() => res) : Promise.resolve(res);
 
 const Head = {
     url: '/x/include/head.html',
