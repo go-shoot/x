1d26a30da77c5922d1a64a2dab6a1c2acecedd0e
bbx <bbx@bbx.com>
2026-03-09 12:53:25 +0800

    Lm

--- include/utilities.js
+++ include/utilities.js
@@ -50,7 +50,7 @@ class Keihin {
             E('div', [
                 E('figure>img', {src, style}), 
                 E('h4', {lang: 'ja'}, [
-                    E('code', code?.replace('-', '‒') || ''), 
+                    E('code', code.includes('?') ? '' : code.replace('-', '‒').replace(/_.+$/, '')), 
                     E('span', jap), 
                     E('small', Markup.spacing(ver?.[0]) ? {
                         classList: ver[0].length > 12 && !ver[0].includes('<br>') ? 'tight' : '',
