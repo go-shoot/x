f0873d79df59c5bf7692cdc98d88200f74d83d73
AEOQ <200552650+AEOQ@users.noreply.github.com>
2026-04-24 13:58:42 +0800

    previewable gear

--- include/utilities.js
+++ include/utilities.js
@@ -71,7 +71,7 @@ class Keihin {
                 E('figure>img', {src, loading: 'lazy', style: typeof style == 'object' ? style : {width: style + '%'}}), 
                 E('h4', {lang: 'ja'}, [
                     E('code', code.includes('?') ? '' : Markup.figure(code).replace(/_.+$/, '')), 
-                    E('a', {href: `//google.com/search?q="${jap}" ${ver?.[0] ?? ''}`, target: '_blank'}, jap), 
+                    E('a', /^\w+$/.test(jap) ? {} : {href: `//google.com/search?q="${jap}" ${ver?.[0] ?? ''}`, target: '_blank'}, jap), 
                     E('small', ver?.[0] ? {
                         classList: ver[0].length > 12 && !ver[0].includes('<br>') ? 'tight' : '',
                         innerHTML: ver[0]
