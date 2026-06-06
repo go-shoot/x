5c66f2b5665d4993dfd6fca7e99ca857235b4e87
bbx <bbx@bbx>
2026-06-06 18:13:00 +0800

    vision

--- identify/index.js
+++ identify/index.js
@@ -1,11 +1,21 @@
 import DB from "../include/DB.js";
+import 'https://cdn.jsdelivr.net/npm/imagehash-web/dist/imagehash-web.min.js';
+
 let PARTS;
 E.img = src => new Promise(res => E('img', {src, onload: function() {res(this)}, onerror: () => res(null)}));
-E.canvas = src => E.img(src).then(img => {
-    let canvas = E('canvas', {width: img.width, height: img.height});
-    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-    return canvas;
-});
+E.canvas = async (img, bg = 'rgb(246,245,250)') => {
+    typeof img == 'string' && (img = await E.img(img));
+    let cvs = E('canvas', {width: img.w ?? img.width, height: img.h ?? img.height});
+    let ctx = cvs.getContext('2d');
+    if (img.x) {
+        ctx.drawImage(Image.collage.img, img.x, img.y, img.w, img.h, 0, 0, img.w, img.h);
+    } else {
+        ctx.fillStyle = bg;
+        ctx.fillRect(0, 0, img.width, img.height);
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+    }
+    return cvs;
+}
 //asset ready first
 const worker = new Worker('worker.js');
 class Collage {
@@ -34,9 +44,9 @@ class Collage {
             ctx.strokeRect(bx, by, bw, bh);
         });
     }
-    tag ({minX, minY}, abbr, ctx = Collage.ctx) {
+    tag ({x0, y0}, abbr, ctx = Collage.ctx) {
         ctx.font = '20px Arial', ctx.fillStyle = 'blue';
-        ctx.fillText(abbr, minX, minY);
+        ctx.fillText(abbr, x0, y0);
     }
     likelyBackdrop () {
         let counts = [...this.boxes].map(points => Collage.rgba(...points.split(','))).reduce((report, rgba) => ({...report, [rgba]: (report[rgba] || 0) + 1}), {});
@@ -62,13 +72,13 @@ const Image = {
                 let {chroma, luma} = Image.calculate.chroluma(data.slice(idx * 4, idx * 4 + 3));
                 if (chroma > App.chroma || luma > App.luma_min && luma < App.luma_max) {
                     let bounds = Image.calculate.boundary(x, y, W, H, data, visited);
-                    let [w, h] = [bounds.maxX - bounds.minX, bounds.maxY - bounds.minY];
+                    let [w, h] = [bounds.x1 - bounds.x0, bounds.y1 - bounds.y0];
                     w > 25 && h > 25 && w < W * 0.2 && h < H * 0.2 && objects.push(bounds);
                 }
             }
         const unnested = [];
-        objects.sort((a, b) => ((b.maxX - b.minX) * (b.maxY - b.minY)) - ((a.maxX - a.minX) * (a.maxY - a.minY)))
-            .forEach(obj => unnested.every(o => obj.maxX < o.minX || obj.minX > o.maxX || obj.maxY < o.minY || obj.minY > o.maxY) && unnested.push(obj));
+        objects.sort((a, b) => ((b.x1 - b.x0) * (b.y1 - b.y0)) - ((a.x1 - a.x0) * (a.y1 - a.y0)))
+            .forEach(obj => unnested.every(o => obj.x1 < o.x0 || obj.x0 > o.x1 || obj.y1 < o.y0 || obj.y0 > o.y1) && unnested.push(obj));
         return unnested;
     }
 }
@@ -78,13 +88,13 @@ Image.calculate = {
         luma: 0.299 * r + 0.587 * g + 0.114 * b
     }),
     boundary (startX, startY, w, h, data, visited) {
-        let [minX, maxX, minY, maxY] = [startX, startX, startY, startY];
-        let stack = [[startX, startY]];
+        let [x0, x1, y0, y1] = [startX, startX, startY, startY];
+        let stack = [[x0, y0]];
         while (stack.length > 0) {
             let [cx, cy] = stack.pop(), idx = cy * w + cx;
             if (cx < 0 || cx >= w || cy < 0 || cy >= h || visited[idx]) continue;
             visited[idx] = 1;
-            cx < minX && (minX = cx); cx > maxX && (maxX = cx); cy < minY && (minY = cy); cy > maxY && (maxY = cy);
+            cx < x0 && (x0 = cx); cx > x1 && (x1 = cx); cy < y0 && (y0 = cy); cy > y1 && (y1 = cy);
     
             [[cx + App.extend, cy], [cx - App.extend, cy], [cx, cy + App.extend], [cx, cy - App.extend]].forEach(([nx, ny]) => {
                 if (!(nx >= 0 && nx < w && ny >= 0 && ny < h)) return;
@@ -93,7 +103,7 @@ Image.calculate = {
                 (chroma > App.chroma || luma > App.luma_min && luma < App.luma_max) && !visited[ny * w + nx] && stack.push([nx, ny]);
             });
         }
-        return { minX, maxX, minY, maxY };
+        return { x0, x1, y0, y1, w: x1-x0, h: y1-y0 };
     },
     embedding: (img, i, backdrop) => img && createImageBitmap(img).then(bitmap => new Promise(res => {
         let handler = ({data}) => i === data.i && [worker.removeEventListener('message', handler), res()];
@@ -107,17 +117,28 @@ const App = () => App.model() && DB.get.essentials({flat: true})
         App.events();
         Q('continuous-knob', knob => knob.dispatchEvent(new InputEvent('input', {bubbles: true})));
         return Promise.all(PARTS.map(P => E.img(`/x/img/${P.path.join('/')}.png`)));
-    }).then(imgs => {
-        Image.assets = imgs.map(img => {
-            if (!img) return {};
-            img = cv.imread(img);
-            let [grayed, rgba] = [new cv.Mat(), new cv.MatVector()];
-            cv.cvtColor(img, grayed, cv.COLOR_RGBA2GRAY);
-            cv.split(img, rgba);
-            let mask = rgba.get(3);//cv.threshold(mask, mask, 50, 255, cv.THRESH_BINARY);
-            img.delete(); rgba.delete();
-            return {matrix: grayed, mask};
-        });
+    })
+    .then(imgs => Promise.all(imgs.map(async img => {
+        if (!img) return {};
+        let hash = E.canvas(img).then(cvs => phash(cvs, 16));
+
+        let colored = cv.imread(img), grayed = new cv.Mat(), rgba = new cv.MatVector();
+        cv.cvtColor(colored, grayed, cv.COLOR_RGBA2GRAY);
+        cv.split(colored, rgba);
+        let mask = rgba.get(3);//cv.threshold(mask, mask, 50, 255, cv.THRESH_BINARY);
+        colored.delete(); rgba.delete();
+
+        let scale = .6;
+        let [w, h] = [Math.floor(grayed.cols * scale), Math.floor(grayed.rows * scale)];
+        let resized = {image: new cv.Mat(), mask: new cv.Mat()};
+        cv.resize(grayed, resized.image, new cv.Size(w, h), 0, 0, cv.INTER_AREA);
+        cv.resize(mask, resized.mask, new cv.Size(w, h), 0, 0, cv.INTER_NEAREST);
+        grayed.delete(); mask.delete();
+
+        return {...resized, hash: await hash};
+    })))
+    .then(assets => {
+        console.log(Image.assets = assets);
         Q('.loading')?.classList.remove('loading');
     });
 
@@ -147,7 +168,10 @@ Object.assign(App, {
     bound () {Image.collage.boxes=new Set();
         Image.collage.draw();
         App.detected = Image.detect();
-        App.detected.forEach(obj => Image.collage.draw(`${obj.minX},${obj.minY},${obj.maxX},${obj.maxY}`, 'green'));
+        App.detected.forEach(obj => {
+            Image.collage.draw(`${obj.x0},${obj.y0},${obj.x1},${obj.y1}`, 'green');
+            Image.collage.boxes.add(`${obj.x0},${obj.y0},${obj.x1},${obj.y1}`);
+        });
         let backdrop = Image.collage.likelyBackdrop();
         if (Image.backdrop != backdrop) {
             Image.backdrop = backdrop;
@@ -159,87 +183,76 @@ Object.assign(App, {
     match () {
         Q('p').classList.add('loading');
         App.results = [];
-        // Image.assets.forEach(({matrix, mask}, i) => {
-        //     if (!matrix) return;
-        //     let scale = .6;
-        //     let [w, h] = [Math.floor(matrix.cols * scale), Math.floor(matrix.rows * scale)];
-        //     let resized = {matrix: new cv.Mat(), mask: new cv.Mat()};
-        //     cv.resize(matrix, resized.matrix, new cv.Size(w, h), 0, 0, cv.INTER_AREA);
-        //     cv.resize(mask, resized.mask, new cv.Size(w, h), 0, 0, cv.INTER_NEAREST);
-
-        //     let result = new cv.Mat();
-        //     cv.matchTemplate(Image.collage.matrix, resized.matrix, result, cv.TM_CCOEFF_NORMED, resized.mask);
-        //     let {maxVal, maxLoc} = cv.minMaxLoc(result);
-        //     if (maxVal > .4) {
-        //         App.results.push({
-        //             scale, w, h,
-        //             x: maxLoc.x,
-        //             y: maxLoc.y,
-        //             score: maxVal.toFixed(3),
-        //             src: matrix.src
-        //         });
-        //         setTimeout(() => {
-        //             Image.collage.draw(`${maxLoc.x},${maxLoc.y},${maxLoc.x+w},${maxLoc.y+h}`);
-        //             Image.collage.tag({minX: maxLoc.x, minY: maxLoc.y}, PARTS[i].abbr);
-        //         });
-        //     }
-        //     console.log(App.results.at(-1));
-        //     resized.matrix.delete(); resized.mask.delete(); result.delete();
-        // })
-        Promise.all(App.detected.map((box, b) => 
-            // hash({x: box.minX, y: box.minY, w: box.maxX-box.minX, h: box.maxY-box.minY})
-            // .then(value => {
-            //     let result = Image.assets.map((h, i) => ({ d: hash.distance(h, value), abbr: PARTS[i].abbr}))
-            //         .sort((a, b) => a.d == null || b.d == null ? 99 : a.d - b.d);
-            //     Image.collage.tag(box, result.slice(0, 2).map(P => P.abbr));
-            //     App.results.push([[box.minX, box.minY, box.maxX, box.maxY], ...result]);
-            // })// &&
-            new Promise(res => {
-                let handler = ({data}) => b === data.b && [worker.removeEventListener('message', handler), res(data.result)];
-                worker.addEventListener('message', handler);
-                worker.postMessage({b, box: [box.minX, box.minY, box.maxX-box.minX, box.maxY-box.minY]}); 
-            }).then(result => {
-                Image.collage.tag(box, PARTS[result[0].p].abbr);
-                App.results.push([[box.minX, box.minY, box.maxX-box.minX, box.maxY-box.minY], ...result.map(({p, score}) => [PARTS[p], score])])
-            })
-        ));
+        //App.match.template();
+        App.match.hash(App.detected);
         Q('.loading')?.classList.remove('loading');
     }
 });
-export default App;
-const hash = (src, xi, yi) => (typeof src == 'string' ? E.img(src) : Promise.resolve(src.x ? Collage.cvs : src))
-    .then(img => {
-        if (!img) return;
-        let [w, h] = [src.w ?? img.width, src.h ?? img.height].map(l => l * (xi == null && yi == null ? 1 : 1/2));
-        let [x, y] = [(src.x ?? 0) + (xi == 0 || xi == null ? 0 : w), (src.y ?? 0) + (yi == 0 || yi == null ? 0 : h)];
-        //let cvs = new OffscreenCanvas(w, h);
-        let cvs = E('canvas', {width: w, height: h});document.body.append(cvs);
-        let ctx = cvs.getContext('2d');
-        if (typeof src == 'string') {
-            ctx.fillStyle = `rgb(246,245,250)`;
-            ctx.fillRect(0, 0, img.width, img.height);
-        }
-        ctx.drawImage(img, x, y, w, h, 0, 0, w, h);
+App.match.template = () => Image.assets.forEach(({image, mask}, i) => {
+    if (!image) return;
+    let result = new cv.Mat();
+    cv.matchTemplate(Image.collage.matrix, image, result, cv.TM_CCOEFF_NORMED, mask);
+    let {maxVal, maxLoc} = cv.minMaxLoc(result);
+    if (maxVal > .4) {
+        App.results.push({
+            x: maxLoc.x, y: maxLoc.y, w: image.cols, h: image.rows,
+            score: maxVal.toFixed(3),
+            src: image.src
+        });
+        setTimeout(() => {
+            Image.collage.draw(`${maxLoc.x},${maxLoc.y},${maxLoc.x+image.cols},${maxLoc.y+image.rows}`);
+            Image.collage.tag({x0: maxLoc.x, y0: maxLoc.y}, PARTS[i].abbr);
+        });
+    }
+    console.log(App.results.at(-1));
+    result.delete();
+});
 
-        let fullHash = '';
-        return xi == null && yi == null ? hash(cvs, 0, 0)
-            .then(h => (fullHash += h) && hash(cvs, 0, 1)).then(h => (fullHash += h) && hash(cvs, 1, 0))
-            .then(h => (fullHash += h) && hash(cvs, 1, 1)).then(h => fullHash += h) : 
-            cvs.convertToBlob?.() ?? new Promise(res => cvs.toBlob(res));
+App.match.hash = boxes => Promise.all(boxes.map(box => 
+    E.canvas({x: box.x0, y: box.y0, w: box.w, h: box.h}).then(cvs => phash(cvs, 16))
+)).then(hashes => 
+    new ResultMatrix('hash', hashes, (h1, h2) => h1.hammingDistance(h2), 5, 'low')
+    .fromOptimum((r, c) => Image.collage.tag(boxes[c], PARTS[r].abbr))
+);
+
+App.match.model = boxes => Promise.all(boxes.map((box, b) => 
+    new Promise(res => {
+        let handler = ({data}) => b === data.b && [worker.removeEventListener('message', handler), res(data.result)];
+        worker.addEventListener('message', handler);
+        worker.postMessage({b, box: [box.x0, box.y0, box.w, box.h]}); 
+    }).then(result => {
+        Image.collage.tag(box, PARTS[result[0].p].abbr);
+        App.results.push([[box.x0, box.y0, box.w, box.h], ...result.map(({p, score}) => [PARTS[p], score])])
     })
-    .then(blob => typeof blob == 'string' ? blob : blob && pHash.hash(new File([blob], "", { type: "image/jpeg" })))
-    .then(hash => typeof hash == 'string' ? hash : hash ? hash.toHex() : null);
-    
-hash.distance = (...hex) => {
-    if (hex.some(h => !h) || hex[0].length != hex[1].length) return null;
-    let distance = 0;
-    for (let i = 0; i < hex[0].length; i++) {
-        let val = hex.map(h => parseInt(h[i], 16));
-        let xor = val[0] ^ val[1];
-        while (xor > 0) {
-            distance++;
-            xor &= xor - 1;
+));
+export default App;
+class ResultMatrix {//row: parts  col: boxes
+    constructor(prop, values, calculator, threshold, type) {
+        rows: for (let [r, asset] of Image.assets.entries()) {
+            this.data[r] = [];
+            if (!asset[prop]) continue rows;
+            for (let [c, v] of values.entries()) {
+                v = calculator(v, asset[prop]);
+                this.data[r].push(v);
+                if (type == 'low' && v < threshold || type == 'high' && v > threshold) {
+                    this.done.rows.add(r) && this.done.cols.add(c);
+                    continue rows;
+                }
+            }
         }
     }
-    return distance;
+    data = []
+    done = {rows: new Set(), cols: new Set()};
+    entries = () => this.data.flatMap((cols, r) => cols.map((value, c) => ({r, c, value})))
+    fromOptimum (callback) {
+        let sorted = this.entries().sort((a, b) => a.value - b.value);
+        sorted.forEach(({ r, c, value }) => {
+            if (this.done.rows.has(r) || this.done.cols.has(c)) return;
+            callback(r, c, value);
+            this.done.rows.add(r) && this.done.cols.add(c);
+        });
+    }
+    toBoxMap () {
+        
+    }
 }
